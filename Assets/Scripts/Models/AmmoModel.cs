213d8624cb35654108fc9d04ea1cb12d04d54808
PxrpleDemxn <malca9616@gmail.com>
2024-01-08 20:49:58 +0100

    Weapon System 1/2

---
+++ Assets/Scripts/Models/AmmoModel.cs
@@ -0,0 +1,7 @@
+public class AmmoModel
+{
+    public ushort Pistol { get; set; } = 60;
+    public ushort Rifle { get; set; }
+    public ushort Shotgun { get; set; }
+    public ushort RPG { get; set; }
+}
---
+++ Assets/Scripts/Models/WeaponModel.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+public class WeaponModel
+{
+    public GameObject Asset { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public ushort Ammo { get; set; }
+}
